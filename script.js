7b9565b9445ccc5d561f1c326eea3fe6a3e89219
Murilo <muripixel6485@gmail.com>
2025-09-03 20:45:18 -0300

    Alterações

---
+++ script.js
@@ -0,0 +1,37 @@
+ const lista = document.getElementById("minhaLista");
+    const btnAdd = document.getElementById("Add");
+    const btnRemove = document.getElementById("remove");
+    const btnLimpar = document.getElementById("Limpar");
+
+    let contador = 1;
+
+    btnAdd.addEventListener("click", () => {
+      const novoItem = document.createElement("li");
+      novoItem.textContent = "Novo item " + contador++;
+      lista.appendChild(novoItem);
+    });
+
+    btnRemove.addEventListener("click", () => {
+      if (lista.lastElementChild) {
+        lista.removeChild(lista.lastElementChild);
+      }
+    });
+
+    btnLimpar.addEventListener("click", () => {
+      lista.innerHTML = "";
+      contador = 1; // resetar contador se quiser
+    });
+
+
+
+
+
+const items = document.querySelectorAll('.faq-item');
+
+    items.forEach(item => {
+      item.addEventListener('click', () => {
+        item.classList.toggle('active');
+      });
+    });
+
+    
